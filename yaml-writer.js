8c8af88112f7c179a7be4e010a6c9b6fdc7a8600
Khang Duong <khang.duong@gmail.com>
2018-03-29 09:47:14 -0700

    Updates to use 2.12 of the fb graph api. Updates to expand multi events.

--- yaml-writer.js
+++ yaml-writer.js
@@ -1,22 +1,34 @@
-var git = require('simple-git')();
-var yaml = require('node-yaml');
-var fb = require('fb');
-var fs = require('fs');
+const git = require('simple-git')();
+const yaml = require('node-yaml');
+const fb = require('fb');
+const fs = require('fs');
 
-var path = 'cpsalsa-site';
+const path = 'cpsalsa-site';
 
 function getEvents(data) {
-    var now = new Date();
+    let list = [...data];
 
-    var events = data.filter(function(event) {
-        var date = new Date(event.start_time);
-        if (date >= now) {
-            return true;
-        }
+    // get the multi events and expand them out
+    let multi = list.find(e => {
+        return e.event_times != null;
+    });
 
-        return false;
-    }).map(function(value) {
-        var item = {
+    for (let times of multi.event_times) {
+        list.push({
+            ...multi,
+            id: times.id,
+            start_time: times.start_time,
+        });
+    }
+
+    // sort the list by date
+    list.sort((a, b) => {
+        return a.start_time < b.start_time ? -1 : a.start_time > b.start_time ? 1 : 0;
+    });
+
+    // finally, convert the list to a format better suited for the yaml
+    let events = list.map(value => {
+        let item = {
             id: value.id,
             title: value.name,
             when: value.start_time,
@@ -60,7 +72,7 @@ function rewriteEvents() {
         client_id: process.env.FB_ID,
         client_secret: process.env.FB_SECRET,
         grant_type: 'client_credentials',
-    }, function(res) {
+    }, res => {
         if(!res || res.error) {
             console.log(!res ? 'error occurred' : res.error);
             return;
@@ -69,21 +81,21 @@ function rewriteEvents() {
         fb.setAccessToken(res.access_token);
 
         // then get the events
-        fb.api('v2.8/cpsalsa/events', { fields: ['id', 'name', 'place', 'cover', 'start_time'] }, function(res) {
+        fb.api('v2.12/cpsalsa/events', {
+            fields: ['id', 'name', 'place', 'cover', 'start_time', 'event_times', 'interested_count'],
+            time_filter: 'upcoming'
+        }, res => {
             if(!res || res.error) {
                 console.log(!res ? 'error occurred' : res.error);
                 return;
             }
 
             // now parse and write to yaml
-            var events = getEvents(res.data);
+            let events = getEvents(res.data);
             if (events.length > 0) {
                 yaml.writeSync(path + '/_data/events.yml', events == null ? { } : events);
-                fs.readFile(path + '/_data/events.yml', { encoding: 'utf8' }, function(err, data) {
-                	console.log(data);
-                })
             } else {
-                var fd = fs.openSync(path + '/_data/events.yml', 'w');
+                let fd = fs.openSync(path + '/_data/events.yml', 'w');
                 fs.writeSync(fd, "", 0);
             }
 
@@ -91,13 +103,13 @@ function rewriteEvents() {
             console.log('committing and pushing');
             git.addConfig('user.name', process.env.USER_NAME)
             	.addConfig('user.email', process.env.USER_EMAIL)
-            	.commit('Updated to latest events from facebook', '_data/events.yml', function(err, data) {
+            	.commit('Updated to latest events from facebook', '_data/events.yml', (err, data) => {
 	            	console.log('commit error:');
 	            	console.log(err);
 	            	console.log('commit data:');
 	            	console.log(data);
 	            })
-            	.push(['--porcelain', 'origin', 'master'], function(err, data) {
+            	.push(['--porcelain', 'origin', 'master'], (err, data) => {
 	            	console.log('data:');
 	            	console.log(data);
 	            });
