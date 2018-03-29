02ad8a302aac1424a73c26e4007aebb59d0fd0c8
Khang Duong <khang.duong@gmail.com>
2018-03-29 10:10:49 -0700

    Adds events from slosx

--- yaml-writer.js
+++ yaml-writer.js
@@ -13,12 +13,14 @@ function getEvents(data) {
         return e.event_times != null;
     });
 
-    for (let times of multi.event_times) {
-        list.push({
-            ...multi,
-            id: times.id,
-            start_time: times.start_time,
-        });
+    if (multi) {
+        for (let times of multi.event_times) {
+            list.push({
+                ...multi,
+                id: times.id,
+                start_time: times.start_time,
+            });
+        }
     }
 
     // sort the list by date
@@ -92,27 +94,41 @@ function rewriteEvents() {
 
             // now parse and write to yaml
             let events = getEvents(res.data);
-            if (events.length > 0) {
-                yaml.writeSync(path + '/_data/events.yml', events == null ? { } : events);
-            } else {
-                let fd = fs.openSync(path + '/_data/events.yml', 'w');
-                fs.writeSync(fd, "", 0);
-            }
 
-            // finally push to repo
-            console.log('committing and pushing');
-            git.addConfig('user.name', process.env.USER_NAME)
-            	.addConfig('user.email', process.env.USER_EMAIL)
-            	.commit('Updated to latest events from facebook', '_data/events.yml', (err, data) => {
-	            	console.log('commit error:');
-	            	console.log(err);
-	            	console.log('commit data:');
-	            	console.log(data);
-	            })
-            	.push(['--porcelain', 'origin', 'master'], (err, data) => {
-	            	console.log('data:');
-	            	console.log(data);
-	            });
+            // get the slosx events
+            fb.api('v2.12/slosx/events', {
+                fields: ['id', 'name', 'place', 'cover', 'start_time', 'event_times', 'interested_count'],
+                // time_filter: 'upcoming'
+            }, res => {
+                let slosxEvents = getEvents(res.data);
+                let combinedEvents = [...events, ...slosxEvents];
+
+                combinedEvents.sort((a, b) => {
+                    return a.when < b.when ? -1 : a.when > b.when ? 1 : 0;
+                });
+
+                if (combinedEvents.length > 0) {
+                    yaml.writeSync(path + '/_data/events.yml', combinedEvents == null ? { } : combinedEvents);
+                } else {
+                    let fd = fs.openSync(path + '/_data/events.yml', 'w');
+                    fs.writeSync(fd, "", 0);
+                }
+
+                // finally push to repo
+                console.log('committing and pushing');
+                git.addConfig('user.name', process.env.USER_NAME)
+                    .addConfig('user.email', process.env.USER_EMAIL)
+                    .commit('Updated to latest events from facebook', '_data/events.yml', (err, data) => {
+                        console.log('commit error:');
+                        console.log(err);
+                        console.log('commit data:');
+                        console.log(data);
+                    })
+                    .push(['--porcelain', 'origin', 'master'], (err, data) => {
+                        console.log('data:');
+                        console.log(data);
+                    });
+            });
         });
     });
 }
